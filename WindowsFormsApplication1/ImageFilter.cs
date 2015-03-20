60a63619f8a34490de9a1490900db6e5b406b834
Nikolay Balandin <nbalandin@dev.rtsoft.ru>
2015-03-20 16:58:13 +0300

    add grayscale filter

---
+++ WindowsFormsApplication1/ImageFilter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication1
+{
+    class ImageFilter
+    {
+        public static Image ConvertToGrayscaleImage(Image image)
+        {
+            var bitmap = new Bitmap(image);
+
+            for (var x = 0; x < bitmap.Width; x++)
+            {
+                for (var y = 0; y < bitmap.Height; y++)
+                {
+                    var pixel = bitmap.GetPixel(x, y);
+                    var gray = (pixel.R + pixel.G + pixel.B) / 3;
+                    bitmap.SetPixel(x, y, Color.FromArgb(pixel.A, gray, gray, gray));
+                }
+            }
+
+            return bitmap;
+        }
+    }
+}

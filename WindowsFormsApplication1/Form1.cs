4871d8cb705cb1f345f1a5c17e104db442f67353
Nikolay Balandin <nbalandin@dev.rtsoft.ru>
2015-03-25 19:24:59 +0300

    Add copyrights

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿/*
+ *  Copyright 2015, RTSoft
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
--- WindowsFormsApplication1/ImageFilter.cs
+++ WindowsFormsApplication1/ImageFilter.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿/*
+ *  Copyright 2015, RTSoft
+ */
+using System;
 using System.Drawing;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,6 +22,23 @@ namespace WindowsFormsApplication1
                     var pixel = bitmap.GetPixel(x, y);
                     var gray = (pixel.R + pixel.G + pixel.B) / 3;
                     bitmap.SetPixel(x, y, Color.FromArgb(pixel.A, gray, gray, gray));
+                }
+            }
+
+            return bitmap;
+        }
+
+        public static Image ConvertToBlackWhiteImage(Image image)
+        {
+            var bitmap = new Bitmap(image);
+
+            for (var x = 0; x < bitmap.Width; x++)
+            {
+                for (var y = 0; y < bitmap.Height; y++)
+                {
+                    var pixel = bitmap.GetPixel(x, y);
+                    var c = (pixel.R + pixel.G + pixel.B) / 3 > 0x80 ? 0: 0xFF;
+                    bitmap.SetPixel(x, y, Color.FromArgb(pixel.A, c, c, c));
                 }
             }
 
